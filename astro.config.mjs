9b5d9d48c84b6c8b4d2d9e61d4f8b817d256e03e
yottajunaid <quadrijunaid369258@gmail.com>
2026-02-24 17:17:57 +0530

    Sorted Sidebar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightCatppuccin from '@catppuccin/starlight'
+import robots from 'astro-robots';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://yottajunaid.github.io',
-  base: '/Master-Darknet',
+  base: '/Master-Darknet/',
 
-  integrations: [
-    starlight({
-      title: 'Master Darknet',
+  integrations: [starlight({
+    title: 'Master Darknet',
 
-      plugins: [starlightCatppuccin({
-          dark: { flavor: "macchiato", accent: "mauve" },
-          light: { flavor: "latte", accent: "mauve" },
-        })],
+    plugins: [starlightCatppuccin({
+        dark: { flavor: "macchiato", accent: "mauve" },
+        light: { flavor: "latte", accent: "mauve" },
+      })],
 
-      social: [
-        {
-          icon: 'github',
-          label: 'GitHub',
-          href: 'https://github.com/yottajunaid/Master-Darknet',
-        },
-      ],
+    social: [
+      {
+        icon: 'github',
+        label: 'GitHub',
+        href: 'https://github.com/yottajunaid/Master-Darknet',
+      },
+    ],
 
-      sidebar: [
-        {
-          label: 'Documentation',
-          autogenerate: { directory: 'guides' },
-          collapsed: true, 
-        },
-      ],
-    }),
-  ],
+    sidebar: [
+      {
+        label: 'Documentation',
+        autogenerate: { directory: 'guides' },
+        collapsed: true, 
+      },
+    ],
+  }), robots(), sitemap()],
 });
